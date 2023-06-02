900b346f438bc49137d196c19bf69ed87938564e
A Bevers <alisabevers@gmail.com>
2023-06-02 09:48:21 -0400

    added urls as comments in thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,15 +9,18 @@ const {
     createReaction
 } = require('../../controllers/thoughtsController');
 
+// localhost:3001/api/thoughts
 router.route('/')
     .get(getThoughts)
     .post(createThought);
 
+// localhost:3001/api/thoughts/:thoughtId
 router.route('/:thoughtId')
     .get(getSingleThought)
     .put(updateThought)
     .delete(deleteThought);
 
+// localhost:3001/api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions')
     .post(createReaction)
 

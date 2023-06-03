401f2b2bfc345f47fb3f1f2007d05394dbd7fe4f
A Bevers <alisabevers@gmail.com>
2023-06-03 16:51:40 -0400

    trying to get the post routes to work

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ module.exports = {
             const users = await User.find();
             res.json(users);
         } catch (err) {
+            console.log(err);
             res.status(500).json(err);
         }
     },
@@ -30,6 +31,7 @@ module.exports = {
             const dbUserData = await User.create(req.body);
             res.json(dbUserData);
         } catch (err) {
+            console.log(err);
             res.status(500).json(err);
         }
     },
